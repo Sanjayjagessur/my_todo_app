77e156ea43a449b3a9a66eeba37f0440e4bad3e7
Sanjayjagessur <sanjayjagessur@gmail.com>
2023-02-16 22:31:49 +0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,15 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, "r") as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
